4a346a92f7c0fdcf6e7fb6cdc2aad42435f78453
Prashant <prashantkalapra@gmail.com>
2026-03-08 20:38:37 +0530

    removed video chat

--- public/script.js
+++ public/script.js
@@ -1,150 +1,71 @@
 const socket = io()
 
-let peer
 let partner = null
-let mode = "text"
-let localStream
 
-const localVideo = document.getElementById("localVideo")
-const remoteVideo = document.getElementById("remoteVideo")
 const messages = document.getElementById("messages")
 
-function msg(text){
-const d=document.createElement("div")
-d.innerText=text
-messages.appendChild(d)
+function addMsg(text){
+
+const div = document.createElement("div")
+div.innerText = text
+
+messages.appendChild(div)
+
+messages.scrollTop = messages.scrollHeight
+
 }
 
-socket.on("users", n=>{
-document.getElementById("users").innerText="Online: "+n
+socket.on("users", count => {
+document.getElementById("users").innerText = "Online: " + count
 })
 
-function startText(){
+function startChat(){
 
-mode="text"
-msg("Searching...")
+addMsg("Searching for stranger...")
+
 socket.emit("find","text")
 
 }
 
-async function startVideo(){
+socket.on("matched", id => {
 
-mode="video"
+partner = id
 
-localStream = await navigator.mediaDevices.getUserMedia({
-video:true,
-audio:true
-})
-
-localVideo.srcObject = localStream
-
-msg("Searching...")
-socket.emit("find","video")
-
-}
-
-socket.on("matched", id=>{
-
-partner=id
-msg("Stranger connected")
-
-if(mode==="video"){
-startPeer(true)
-}
-
-})
-
-function startPeer(initiator){
-
-peer = new RTCPeerConnection({
-iceServers:[
-{urls:"stun:stun.l.google.com:19302"}
-]
-})
-
-localStream.getTracks().forEach(t=>{
-peer.addTrack(t,localStream)
-})
-
-peer.ontrack=e=>{
-remoteVideo.srcObject=e.streams[0]
-}
-
-peer.onicecandidate=e=>{
-if(e.candidate){
-socket.emit("signal",{to:partner,data:e.candidate,type:"candidate"})
-}
-}
-
-if(initiator){
-
-peer.createOffer().then(o=>{
-peer.setLocalDescription(o)
-socket.emit("signal",{to:partner,data:o,type:"offer"})
-})
-
-}
-
-}
-
-socket.on("signal", async d=>{
-
-if(d.type==="offer"){
-
-startPeer(false)
-
-await peer.setRemoteDescription(d.data)
-
-const ans = await peer.createAnswer()
-await peer.setLocalDescription(ans)
-
-socket.emit("signal",{to:partner,data:ans,type:"answer"})
-
-}
-
-if(d.type==="answer"){
-await peer.setRemoteDescription(d.data)
-}
-
-if(d.type==="candidate"){
-peer.addIceCandidate(d.data)
-}
+addMsg("Connected to stranger")
 
 })
 
 function sendMessage(){
 
-const input=document.getElementById("msg")
+const input = document.getElementById("msg")
 
 if(!input.value) return
 
-msg("You: "+input.value)
+addMsg("You: " + input.value)
 
-socket.emit("message",{to:partner,msg:input.value})
+socket.emit("message",{
+to: partner,
+msg: input.value
+})
 
-input.value=""
+input.value = ""
 
 }
 
-socket.on("message", m=>{
-msg("Stranger: "+m)
+socket.on("message", msg => {
+
+addMsg("Stranger: " + msg)
+
 })
 
 function nextUser(){
 
-if(peer){
-peer.close()
-peer=null
-}
-
-remoteVideo.srcObject=null
-
-msg("Searching new stranger...")
+addMsg("Finding new stranger...")
 
 socket.emit("next")
 
 setTimeout(()=>{
-socket.emit("find",mode)
+socket.emit("find","text")
 },500)
 
 }
