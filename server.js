750116d4c1508653cef2e21bb9ce4f8e6a252169
Prashant <prashantkalapra@gmail.com>
2026-03-08 18:45:48 +0530

    second commit

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log("Server running")
+})
+
 app.use(express.static(__dirname))
 
 let waitingUser = null
